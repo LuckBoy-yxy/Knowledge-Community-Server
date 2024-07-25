3f3db494e6de8bb9390d3798b7d49b43de3d9423
yuanxiaoran <hmxs_hmbb@163.com>
2024-07-25 11:51:24 +0800

    WebSocket 心跳检测 bug 已解决

--- src/config/WebSocket.js
+++ src/config/WebSocket.js
@@ -56,7 +56,7 @@ class WebSocketServer {
       },
       heartbeat: () => {
         console.log(msgObj)
-        if (msgObj.messgae === 'pong') {
+        if (msgObj.message === 'pong') {
           ws.isAlive = true
         }
       },
@@ -99,6 +99,7 @@ class WebSocketServer {
     clearInterval(this.interval)
     this.interval = setInterval(() => {
       this.wss.clients.forEach(ws => {
+        console.log('server', ws.isAlive)
         if (!ws.isAlive) {
           return ws.terminate()
         }

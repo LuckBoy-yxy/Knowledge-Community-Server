0b36287da34b4fee70ac797cc970ca782d686d2b
yuanxiaoran <hmxs_hmbb@163.com>
2024-12-08 20:05:27 +0800

    在获取错误日志列表数据接口中, 追加 '错误状态码' 和 '请求类型' 的筛选查询逻辑已完成

--- src/api/ErrorController.js
+++ src/api/ErrorController.js
@@ -1,12 +1,21 @@
 import ErrorRecord from '../model/ErrorRecord'
+import qs from 'qs'
 
 class ErrorController {
   async getErrorList (ctx) {
-    const query = {}
+    let query = {}
     const params = ctx.query
+    const obj = qs.parse(params)
 
     const page = params.page ? +params.page - 1 : 0
     const pageSize = params.pageSize ? +params.pageSize : 10
+    query = obj.filter ? { ...obj.filter } : {}
+    if (query.method) {
+      query.method = {
+        $regex: query.method,
+        $options: 'i'
+      }
+    }
 
     const result = await ErrorRecord.find(query)
      .skip(page * pageSize)
@@ -14,11 +23,49 @@ class ErrorController {
      .sort({ created: -1 })
     const total = await ErrorRecord.find(query).countDocuments()
 
+    const codeFilter = await ErrorRecord.aggregate([
+      {
+        $group: {
+          _id: '$code'
+        }
+      },
+      {
+        $sort: { _id: 1 }
+      }
+    ])
+
+    const methodFilter = await ErrorRecord.aggregate([
+      {
+        $group: {
+          _id: '$method'
+        }
+      },
+      {
+        $sort: { _id: 1 }
+      }
+    ])
+
     ctx.body = {
       code: 200,
       msg: '查询成功',
       data: result,
-      total: total
+      total: total,
+      filters: {
+        // method: methodFilter,
+        method: methodFilter.map(item => {
+          return {
+            label: item._id,
+            value: item._id.toLowerCase()
+          }
+        }),
+        // code: codeFilter
+        code: codeFilter.map(item => {
+          return {
+            label: item._id,
+            value: +item._id
+          }
+        })
+      }
     }
   }
 
